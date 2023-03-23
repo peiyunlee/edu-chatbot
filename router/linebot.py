9551b7ef95a97a88071d6b8015e41ce9ba4f277c
Peiyun Lee <may1092200258@gmail.com>
2023-03-23 13:37:43 +0800

    feat: enforce push message

--- router/linebot.py
+++ router/linebot.py
@@ -128,6 +128,12 @@ async def enforce_push_Q(line_group_id: str, hw_no_now: int, group_id: int, line
     return JSONResponse(status_code=status.HTTP_200_OK, content="success", headers=header)
 
 
+@router.post('/push/message/{line_user_id}', summary="傳送任意訊息給指定用戶")
+async def enforce_push_message(line_user_id: str, text: str):
+    line_bot_api.push_message(to=line_user_id, messages=TextMessage(text))
+    return JSONResponse(status_code=status.HTTP_200_OK, content="success", headers=header)
+
+
 
 # ---------------------------------------------------------- 群組
 
